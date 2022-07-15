855bc667e9a7dc4f88546ddc82b24d2ccf07fadc
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-07-15 10:52:32 +0700

    update post route

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,9 +1,12 @@
 import express,{ Router } from 'express';
 import { getPost, createPost } from '../controllers/post.controller';
+import { vertifyToken } from '../middlewares/vertifyHeader';
 
 const router: Router = express.Router();
 
 router.get('/', getPost);
+
+router.use(vertifyToken);
 router.post('/', createPost)
 
 export { router as PostRoute }
