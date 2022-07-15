06f492f38f9f12b43403f9b57e221c6963d06f81
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-07-15 10:54:26 +0700

    update root

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,38 @@ import * as dotenv from 'dotenv';
 import * as routes from './routes';
 import cors from 'cors';
 import { connectMongoDB } from './utilities/database';
+import expressSession from 'express-session';
 
 dotenv.config();
 
 const app: Application = express();
+
+
+declare module 'express-session' {
+    interface SessionData {
+        uid: string | null;
+        [key: string]: any;
+    }
+}
+
+
+const sessionOptions: expressSession.SessionOptions = {
+    name: 'session',
+    secret: "khohuai",
+    saveUninitialized: false,
+    resave: false,
+    cookie: { 
+        secure: false,
+        sameSite: 'lax', 
+        maxAge: 1000 * 60 * 60
+    }
+}
+app.use(expressSession(sessionOptions))
 app.use(express.json())
 app.use(cors({ origin: ["*"] }))
 
 app.use('/auth', routes.AuthRoute);
+app.use('/user', routes.userRoute);
 app.use('/post', routes.PostRoute);
 
 const port: number = Number(process.env.PORT!);
