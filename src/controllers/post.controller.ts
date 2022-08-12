566a3db90f6a5b60e63c2c283e2bd964de1544d6
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-08-12 22:15:22 +0700

    edit post controller

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import Post from '../models/Post.model';
+import { uploadImage } from '../utilities/multer'
 
 export const getPost = async (req: Request, res: Response,) => {
     try {
         let posts = await Post.find()
-                                .populate({path: 'author', model: 'users'});
+            .populate({ path: 'author', model: 'users' });
 
         res.status(200).json({ data: posts });
     } catch (error) {
@@ -14,10 +15,10 @@ export const getPost = async (req: Request, res: Response,) => {
 
 export const getPostByUser = async (req: Request, res: Response) => {
     const uid = req.params.uid;
-    console.log("uid",uid);
+    console.log("uid", uid);
     try {
-        let posts = await Post.find({author: uid})
-                                .populate({path: 'author', model: 'users'});
+        let posts = await Post.find({ author: uid })
+            .populate({ path: 'author', model: 'users' });
 
         res.status(200).json({ data: posts });
     } catch (error) {
@@ -27,16 +28,22 @@ export const getPostByUser = async (req: Request, res: Response) => {
 
 export const createPost = async (req: Request, res: Response) => {
 
-    const { body } = req;
-
-    const data = {
-        ...body,
-        author: req.session.uid
-    }
+    const { text, tags, picture}  = req.body
 
     try {
+
+        const { secure_url } = await uploadImage(picture);
+
+        const picture_name = secure_url.split('/').pop();
+        
+        const data = {
+            text,
+            tags,
+            picture_name,
+            author: req.session.uid
+        }
         await Post.create(data);
-        res.status(201).json({ message: 'created' });
+        res.status(201).json({ message: 'created', data });
     } catch (error) {
         console.log(error);
     }
